239cd991a6910433d3c02ed39c5bbd7e8e549c6f
cgolinski <cgolinski@users.noreply.github.com>
2020-04-08 18:02:11 -0400

    sets up graphql

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import ApolloClient, { gql } from "apollo-boost";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+      <App />
+      <ExchangeRates />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
