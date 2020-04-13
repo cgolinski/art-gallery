925c19e2684294cfdc1f85711effdc60d7363897
cgolinski <cgolinski@users.noreply.github.com>
2020-04-13 01:03:59 -0400

    sets up getArtworkDetail query

--- src/ArtworkDetail.component.tsx
+++ src/ArtworkDetail.component.tsx
@@ -1,13 +1,58 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import { HomeButton } from './HomeButton.component';
+import { gql } from 'apollo-boost';
+import { GetArtworkDetailData } from './types/data.types';
+import { Loading } from './Loading.component';
+import { Error } from './Error.component';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_ARTWORK_DETAIL = gql`
+  query getArtworkDetail($id: String!) {
+    artwork(id: $id) {
+      id
+      artist_names
+      category
+      collecting_institution
+      date
+      description
+      dimensions {
+        in
+      }
+      imageUrl
+      image_title
+      displayLabel
+      medium
+      title
+    }
+  }
+`;
 
 export const ArtworkDetailComponent = () => {
   const { artworkId } = useParams();
+
+  const { loading, error, data } = useQuery<GetArtworkDetailData>(
+    GET_ARTWORK_DETAIL,
+    {
+      variables: { id: artworkId },
+    }
+  );
+
+  const artworkDetail = data?.artwork;
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error errorMessage={error?.message} />;
+  }
+
   return (
     <div>
       <HomeButton />
       <div>Artwork page. artworkId: {artworkId}</div>
+      artworkDetail: {console.log({ artworkDetail })}
     </div>
   );
 };
--- src/types/data.types.ts
+++ src/types/data.types.ts
@@ -11,8 +11,28 @@ export type Artists = Artist[];
 
 export type PopularArtists = { artists: Artists };
 
+export type ArtworkDetail = {
+  id: string;
+  artist_names: string;
+  category: string;
+  collecting_institution: string;
+  date: string;
+  description: string;
+  dimensions: {
+    in: string;
+  };
+  imageUrl: string;
+  image_title: string;
+  displayLabel: string;
+  medium: string;
+  title: string;
+};
+
 export type GetPopularArtistsData = {
   popular_artists: PopularArtists;
+};
+export type GetArtworkDetailData = {
+  artwork: ArtworkDetail;
 };
 
 export type ArtworkTileType = {
