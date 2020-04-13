fa7178fa0f5303073180db70f3620a32e33eddb7
cgolinski <cgolinski@users.noreply.github.com>
2020-04-13 13:27:50 -0400

    adds frames to gallery view

--- src/ArtworkTile.component.tsx
+++ src/ArtworkTile.component.tsx
@@ -20,8 +20,20 @@ export const ArtworkTile: React.StatelessComponent<ArtworkTileProps> = ({
     <div
       className="ba pa4 ma4 flex flex-column bg-near-white pointer"
       onClick={handleClick}
+      style={{
+        border: 'solid 4vmin #222',
+        borderBottomColor: '#444',
+        borderLeftColor: '#333',
+        borderRadius: '2px',
+        borderRightColor: '#333',
+        borderTopColor: '#222',
+        boxShadow:
+          '0 0 5px 0 rgba(0,0,0,.25) inset, 0 5px 10px 5px rgba(0,0,0,.25)',
+        boxSizing: 'border-box',
+      }}
     >
-      <img src={artworkTile.imageUrl} alt="imageAltTag" />
+      <img src={artworkTile.imageUrl} alt="artwork" />
+      {/* <img src={artworkTile.images[0].url} alt="artwork-square" /> */}
       <span className="pt2">{artworkTile.title}</span>
     </div>
   );
