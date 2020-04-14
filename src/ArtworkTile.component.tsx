980563016a493edf06c56b6946eddce7b743fde0
cgolinski <cgolinski@users.noreply.github.com>
2020-04-13 22:24:18 -0400

    updates and separates ArtworkTile styles

--- src/ArtworkTile.component.tsx
+++ src/ArtworkTile.component.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import type { ArtworkTileType } from './types/data.types';
 import { NavLink } from 'react-router-dom';
+import './ArtworkTile.component.css';
+import type { ArtworkTileType } from './types/data.types';
 
 type ArtworkTileProps = {
   key: string;
@@ -11,22 +12,8 @@ export const ArtworkTile: React.StatelessComponent<ArtworkTileProps> = ({
   artworkTile,
 }: ArtworkTileProps) => {
   return (
-    <NavLink to={`/artwork/${artworkTile.id}`}>
-      <div
-        className="ba pa4 ma4 tc bg-near-white hover-bg-lightest-blue pointer grow"
-        style={{
-          border: 'solid 4vmin #222',
-          borderBottomColor: '#444',
-          borderLeftColor: '#333',
-          borderRadius: '2px',
-          borderRightColor: '#333',
-          borderTopColor: '#222',
-          boxShadow:
-            '0 0 5px 0 rgba(0,0,0,.25) inset, 0 5px 10px 5px rgba(0,0,0,.25)',
-          boxSizing: 'border-box',
-          transition: 'all 0.12s ease-in',
-        }}
-      >
+    <NavLink to={`/artwork/${artworkTile.id}`} className="artwork-link">
+      <div className="ma4 pointer picture-frame artwork-tile">
         <img src={artworkTile.imageUrl} alt="artwork" />
       </div>
     </NavLink>
