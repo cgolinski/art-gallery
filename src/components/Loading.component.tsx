400f35a1606a4671c7ad101f4e15fc4df0b79561
cgolinski <cgolinski@users.noreply.github.com>
2020-04-14 10:24:37 -0400

    fixes page jumping when loading spinner

--- src/components/Loading.component.tsx
+++ src/components/Loading.component.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import './Loading.component.css';
 
 export const Loading: React.StatelessComponent = () => (
-  <div className="loading-spinner">
-    <span role="img" aria-label="loading-spinner" className="loading-spinner">
-      🎨
-    </span>
+  <div className="wrapper">
+    <div className="loading-spinner">
+      <span role="img" aria-label="loading-spinner">
+        🎨
+      </span>
+    </div>
   </div>
 );
