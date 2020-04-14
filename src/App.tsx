be959737b322e411b169a78a74b04e62eab4e7bb
cgolinski <cgolinski@users.noreply.github.com>
2020-04-14 09:40:46 -0400

    moves trending_artists query from App to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
-import { GetTrendingArtistsData } from './types/data.types';
 import { ArtworkDetail } from './components/ArtworkDetail.component';
 import { Dashboard } from './components/Dashboard.component';
 import { AppHeader } from './components/AppHeader.component';
 
-const GET_TRENDING_ARTISTS = gql`
-  {
-    trending_artists(name: ARTIST_FOLLOW) {
-      artists {
-        id
-        name
-        birthday
-        deathday
-        artworks {
-          id
-          images {
-            url(version: "square")
-          }
-        }
-      }
-    }
-  }
-`;
-
 const App = () => {
-  const { loading, error, data } = useQuery<GetTrendingArtistsData>(
-    GET_TRENDING_ARTISTS
-  );
-
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const trendingArtistsData: GetTrendingArtistsData = data || {
-    trending_artists: { artists: [] },
-  };
-
-  const displayedArtists = trendingArtistsData.trending_artists.artists.filter(
-    ({ name }) => name.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
   return (
     <Router>
       <AppHeader />
@@ -49,13 +13,7 @@ const App = () => {
           <ArtworkDetail />
         </Route>
         <Route path="/">
-          <Dashboard
-            setSearchValue={setSearchValue}
-            searchValue={searchValue}
-            loading={loading}
-            error={error}
-            displayedArtists={displayedArtists}
-          />
+          <Dashboard />
         </Route>
       </Switch>
     </Router>
--- src/components/Dashboard.component.tsx
+++ src/components/Dashboard.component.tsx
@@ -1,22 +1,54 @@
 import * as React from 'react';
 import { Gallery } from './Gallery.component';
 import { Search } from './Search.component';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import { GetTrendingArtistsData } from '../types/data.types';
 
-export const Dashboard = ({
-  setSearchValue,
-  searchValue,
-  loading,
-  error,
-  displayedArtists,
-}: any) => (
-  <div className="pa3">
-    <div className="tc pt4">
-      <Search setSearchValue={setSearchValue} searchValue={searchValue} />
+const GET_TRENDING_ARTISTS = gql`
+  {
+    trending_artists(name: ARTIST_FOLLOW) {
+      artists {
+        id
+        name
+        birthday
+        deathday
+        artworks {
+          id
+          images {
+            url(version: "square")
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Dashboard = () => {
+  const { loading, error, data } = useQuery<GetTrendingArtistsData>(
+    GET_TRENDING_ARTISTS
+  );
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const trendingArtistsData: GetTrendingArtistsData = data || {
+    trending_artists: { artists: [] },
+  };
+
+  const displayedArtists = trendingArtistsData.trending_artists.artists.filter(
+    ({ name }) => name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  return (
+    <div className="pa3">
+      <div className="tc pt4">
+        <Search setSearchValue={setSearchValue} searchValue={searchValue} />
+      </div>
+      <Gallery
+        loading={loading}
+        error={error}
+        displayedArtists={displayedArtists}
+      />
     </div>
-    <Gallery
-      loading={loading}
-      error={error}
-      displayedArtists={displayedArtists}
-    />
-  </div>
-);
+  );
+};
