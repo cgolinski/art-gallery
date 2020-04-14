7287cee24bca38be5dc7f84b2f6897b1cb69cd47
cgolinski <cgolinski@users.noreply.github.com>
2020-04-13 23:53:55 -0400

    changes loading spinner image

--- src/components/Loading.component.tsx
+++ src/components/Loading.component.tsx
@@ -2,5 +2,9 @@ import * as React from 'react';
 import './Loading.component.css';
 
 export const Loading: React.StatelessComponent = () => (
-  <div className="loading-spinner"></div>
+  <div className="loading-spinner">
+    <span role="img" aria-label="loading-spinner" className="loading-spinner">
+      🎨
+    </span>
+  </div>
 );
