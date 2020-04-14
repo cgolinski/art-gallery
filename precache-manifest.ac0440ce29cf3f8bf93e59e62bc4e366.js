07b95c48d35b55c23dd2c5d77bb4a30a9ce3f8df
cgolinski <cgolinski@users.noreply.github.com>
2020-04-14 11:21:06 -0400

    Updates

---
+++ precache-manifest.ac0440ce29cf3f8bf93e59e62bc4e366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89798245f2da73b02004247a01079df4",
+    "url": "/art-gallery/index.html"
+  },
+  {
+    "revision": "bf0fa00594050e87150c",
+    "url": "/art-gallery/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "afc28e63da75d1d83275",
+    "url": "/art-gallery/static/css/main.47a06c32.chunk.css"
+  },
+  {
+    "revision": "bf0fa00594050e87150c",
+    "url": "/art-gallery/static/js/2.2fc9a9d7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/art-gallery/static/js/2.2fc9a9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc28e63da75d1d83275",
+    "url": "/art-gallery/static/js/main.d1d9247c.chunk.js"
+  },
+  {
+    "revision": "9cb01ee6c712a33f8bea",
+    "url": "/art-gallery/static/js/runtime-main.aaba4fd3.js"
+  }
+]);
